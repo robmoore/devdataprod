f2e053b5631e267e74e2305e796b3366845c26b7
robmoore <rob.moore@gmail.com>
2015-07-25 16:00:56 -0500

    Added logic to display historical information regarding the passengers. Added background/explanatory text.

--- server.R
+++ server.R
@@ -1,26 +1,40 @@
 library(scales)
 
 data(Titanic)
+titanicDF <- as.data.frame(Titanic)
 model <- glm(Survived ~ Class + Sex + Age, 
-             data = as.data.frame(Titanic), 
-             weights = Freq, family = binomial)
+             data = titanicDF,
+             weights = Freq, 
+             family = binomial)
 
 shinyServer(
   function(input, output) {
     output$Prediction <- renderText({
-      prediction <- predict(model, 
-                            newdata = data.frame(Class = input$Class, Sex = input$Sex, Age = input$Age), 
-                            type = "response")
-      c("As", 
-        if (input$Age == 'Child') " a " else " an ",
-        tolower(input$Age), 
-        " ", 
-        tolower(input$Sex), 
-        " ", 
+      prediction <- percent(predict(model, 
+                                    newdata = data.frame(Class = input$Class, 
+                                                         Sex = input$Sex, 
+                                                         Age = input$Age), 
+                                    type = 'response'))
+      survived <- titanicDF[titanicDF$Class == input$Class & 
+                              titanicDF$Sex == input$Sex & 
+                              titanicDF$Age == input$Age & 
+                              titanicDF$Survived == 'Yes', 
+                            'Freq']
+      perished <- titanicDF[titanicDF$Class == input$Class & 
+                              titanicDF$Sex == input$Sex & 
+                              titanicDF$Age == input$Age & 
+                              titanicDF$Survived == 'No', 
+                            'Freq']
+      reality <- ifelse(survived + perished != 0, 
+                      paste('the percentage of survivors with these characteristics was', percent(survived / (survived + perished))), 
+                      'no passengers with this combination of characteristics is found in the data')
+      c('As', 
+        ifelse(input$Age == 'Child', 'a', 'an'),
+        tolower(input$Age),
+        tolower(input$Sex),
         tolower(input$Class), 
-        " class passenger of the Titanic, the probability that you would survive was ",
-        percent(prediction),
-        ".")
+        paste0('class passenger of the Titanic, our model suggests that the probability that you would survive was ', prediction, '.'),
+        paste0('In actual fact, ', reality, '.'))
     })
   }
 )
--- ui.R
+++ ui.R
@@ -13,6 +13,8 @@ shinyUI(pageWithSidebar(
   mainPanel(
     tags$img(src="http://i122.photobucket.com/albums/o250/blackroutes/Scans/PeopleTakeWarningTitanic.jpg",
              width = "50%"),
+    tags$h4("In April 1912, arguably one of the most infamous maritime disasters in history occurred with the sinking of the RMS Titanic."),
+    tags$h4("Using a data set of produced by the British Board of Trade in their investigation of the tragedy, we employ logistic regression to calculate the likelihood that a passenger with a specific combination of characteristics would have survived the sinking of the Titanic."),
     tags$h4(textOutput("Prediction"))
   )
 )
